84d89cf4d418f51c14c747ea6190619fae1938cf
Mercrist <yariel.then@gmail.com>
2022-02-27 16:23:28 -0400

    Tests template

---
+++ Classes/test_orders.py
@@ -0,0 +1,22 @@
+import unittest
+
+class TestReceipt(unittest.TestCase):
+    '''Tests the Receipt class along with its methods.'''
+    def test_types(self):
+        pass
+
+    def test_values(self):
+        pass
+
+
+class TestCart(unittest.TestCase):
+    '''Tests the Receipt class along with its methods.'''
+    def test_types(self):
+        pass
+
+    def test_values(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
