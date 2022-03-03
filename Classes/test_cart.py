dfa51289ceb084fafcc20bea3c513756a095c1b1
XV1R <xavytron@gmail.com>
2022-03-03 12:43:44 -0400

    fixed test syntax

--- Classes/test_cart.py
+++ Classes/test_cart.py
@@ -11,22 +11,23 @@ class TestShoppingCart(unittest.TestCase):
         self.cart2.add_items('Americano',3)
         self.cart2.set_reservation('Sunday','12:00','PM')
     def test_types(self):
-        self.assertRaises(TypeError,self.cart.add_items(10,5))
-        self.assertRaises(TypeError, self.cart.add_items("Espresso","1"))
-        self.assertRaises(TypeError, self.cart.set_reservation()) #TODO 
-        self.assertRaises(TypeError, self.remove_items(1))
+        self.assertRaises(TypeError,self.cart.add_items,10,5)
+        self.assertRaises(TypeError, self.cart.add_items,"Espresso","1")
+        self.assertRaises(TypeError, self.cart.set_reservation,None) #TODO 
+        self.assertRaises(TypeError, self.remove_items,1)
         self.assertRaises(TypeError, ShoppingCart, 10)
 
     def test_values(self):
-        self.assertRaises(ValueError, self.cart.add_items("Espresso",-1))
-        self.assertRaises(ValueError, self.cart.add_items("Not an item",1))
-        self.assertRaises(ValueError, self.cart.add_items("New York Cheesecake", 2))
-        self.assertRaises(ValueError, self.cart.remove_items("Item not in cart", 1))
-        self.assertRaises(ValueError, self.cart.remove_items("Espresso", 10)) #remove more espressos than in cart
-        self.assertRaises(ValueError, self.cart.set_reservation("Saturday","1:00","AM"))
-        self.assertRaises(ValueError, self.cart.set_reservation("Monday", "4:00", "AM"))
-        self.assertRaises(ValueError, self.cart.set_reservation("twensday", "4:00","PM"))
-        self.assertRaises(ValueError, self.cart.set_reservation("Monday","100:00", "AM"))
+        self.assertRaises(ValueError, ShoppingCart, "")
+        self.assertRaises(ValueError, self.cart.add_items,"Espresso",-1)
+        self.assertRaises(ValueError, self.cart.add_items,"Not an item",1)
+        self.assertRaises(ValueError, self.cart.add_items,"New York Cheesecake", 2)
+        self.assertRaises(ValueError, self.cart.remove_items,"Item not in cart", 1)
+        self.assertRaises(ValueError, self.cart.remove_items,"Espresso", 10) #remove more espressos than in cart
+        self.assertRaises(ValueError, self.cart.set_reservation,"Saturday","1:00","AM")
+        self.assertRaises(ValueError, self.cart.set_reservation,"Monday", "4:00", "AM")
+        self.assertRaises(ValueError, self.cart.set_reservation,"twensday", "4:00","PM")
+        self.assertRaises(ValueError, self.cart.set_reservation,"Monday","100:00", "AM")
 
 
     def test_cart_attributes(self):
