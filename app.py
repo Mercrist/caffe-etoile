884ae47e183c712b03a4052c558ea3f2d078f223
Mercrist <yariel.then@gmail.com>
2022-03-21 21:29:05 -0400

    Index refactored

--- app.py
+++ app.py
@@ -2,3 +2,21 @@ from flask import Flask, url_for, redirect, request, render_template
 import pymongo
 
 app = Flask(__name__) #initialize flask app
+
+'--Routes--'
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return "Placeholder"
+
+@app.route('/contact')
+def contact():
+    return "Placeholder"
+
+@app.route('/menu')
+def menu():
+    return "Placeholder"
