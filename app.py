82bc2529143ee414376eb74112b7e06c44ca1d68
XV1R <xavier@techexchange.in>
2022-03-23 02:34:47 +0000

    converted menu.html to use jinja templating

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for, redirect, request, render_template
 from flask_pymongo import PyMongo
+from Classes.Statics import menu as cafe_menu
+from Classes.Statics import MenuItem
 
 app = Flask(__name__) #initialize flask app
 
@@ -19,4 +21,9 @@ def contact():
 
 @app.route('/menu')
 def menu():
-    return render_template("menu.html")
+    return render_template("menu.html",
+    coffees=[item for item in cafe_menu.values() if item.category == "Coffee"],
+    sandwiches=[item for item in cafe_menu.values() if item.category == "Sandwiches"],
+    pastries=[item for item in cafe_menu.values() if item.category == "Pastries"],
+    desserts=[item for item in cafe_menu.values() if item.category == "Desserts"],
+    specialties=[item for item in cafe_menu.values() if item.category == "Specialty"])
