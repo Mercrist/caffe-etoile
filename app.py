0b9ce08bef42f9fff407b8c5c7520c0a7aed8ebe
XV1R <xavytron@gmail.com>
2022-04-03 20:27:58 -0400

    menu images now fit wrapper; item prices are now shown in receipt

--- app.py
+++ app.py
@@ -47,17 +47,13 @@ def contact():
 def menu():
     if request.args.get('checked_out') or request.args.get('search'):
         receipt_number = request.args.get('receipt')
-        print(receipt_number)
-        print(type(receipt_number))
-
         receipt = db.receipts.find_one({"receipt_number":receipt_number})
-        print(receipt)
-        print(type(receipt))
         return render_template("menu.html", 
             database=db, 
             categories=model.categories,
             checked_out=True,
-            receipt=receipt)
+            receipt=receipt,
+            menu=db.menu)
         
     if request.args.get('added'):
         flash('Added items to cart!','info')
@@ -179,33 +175,25 @@ def admin(username):
 @app.route("/order",methods=['GET','POST'])
 def order():
     if "cart" not in session:
-        flash("There's nothing in your cart!","danger")
         session['cart'] = vars(ShoppingCart("User"))
-
     return render_template("order.html", cart=session['cart'])
 
 @app.route('/add_to_cart/<string:item_name>',methods=['POST'])
 def add_to_cart(item_name):
     if 'cart' not in session:
-        print('CART NOT FOUND')
         session['cart'] = vars(ShoppingCart("User"))
 
     amount = int(request.form['amount'])
-    print(session['cart'])
     cart = json_to_cart(session['cart'])
-    print(cart)
     cart.add_items(item_name,amount)
     session.pop('cart')
     session['cart'] = vars(cart)
     session.modified = True
-    print("added to cart!")
-    print(session['cart'])
 
     return redirect(url_for("menu",added=True))
 
 @app.route('/checkout',methods=["POST"])
 def checkout():
-    print(request.form)
     cart = json_to_cart(session['cart']) 
     if request.form['firstName']:
         cart.name = request.form['firstName']
@@ -214,15 +202,12 @@ def checkout():
         hour = request.form['reservationHour']
         meridiem = request.form['reservationMeridiem']
         cart.set_reservation(day,hour,meridiem)
-        print(cart)
 
     receipt = Receipt(cart.cart,cart.reservation,cart.name,cart.subtotal)
-    print(receipt)
     if 'receipts' not in db.list_collection_names():
         db.create_collection('receipts')
     json_receipt = receipt_to_json(receipt)
     db.receipts.insert_one(json_receipt) 
-    print([receipt for receipt in db.receipts.find({})])
     if request.form.get('downloadReceipt'):
         receipt.generate_receipt()
 
